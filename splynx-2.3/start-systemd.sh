47c960d73e9ae0e9e53fae2dc6febf33cfa33593
Peter Pliuta <peter@splynx.com.ua>
2019-04-18 16:17:45 +0300

    rm 2.3

--- splynx-2.3/start-systemd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-dpkg-reconfigure -fnoninteractive splynx >>/var/www/splynx/docker/log.log 2>&1
-/var/www/splynx/system/script/passwd admin --login=admin --password=docker >>/var/www/splynx/docker/log.log 2>&1
