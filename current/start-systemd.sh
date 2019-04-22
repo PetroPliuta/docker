6572080648c8ac83dcdc0c95a30dcaf7ebb595b7
Peter Pliuta <peter@splynx.com.ua>
2019-04-22 16:25:49 +0300

    service changings

--- current/start-systemd.sh
+++ current/start-systemd.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
-dpkg-reconfigure -fnoninteractive splynx >>/var/www/splynx/docker/log.log 2>&1
-/var/www/splynx/system/script/passwd admin --login=admin --password=docker >>/var/www/splynx/docker/log.log 2>&1
+license_length=`grep license /var/www/splynx/config/config.php | grep -v ^# | tail -n 1 | cut -d= -f2 | tr -d "\n\r\" " | wc -m`
+if ! [ $license_length -eq "32" ]; then
+    init
+fi
+
+redis_pass=`grep -i requirepass /etc/redis/redis.conf | grep -v ^# | tail -n 1 | cut -d\  -f2 | tr -d "\n\r\" "`
+splynx_redis_pass=`grep -i password /var/www/splynx/config/redis.php | grep -v ^# | tail -n 1 | cut -d= -f2 | tr -d "\n\r\" "`
+if ! [ $redis_pass -eq $splynx_redis_pass ]; then
+    redis_setup
+fi
+
+init()
+{
+    dpkg-reconfigure -fnoninteractive splynx >>/var/www/splynx/docker/log.log 2>&1
+    /var/www/splynx/system/script/passwd admin --login=admin --password=docker >>/var/www/splynx/docker/log.log 2>&1
+}
+
+redis_setup()
+{
+    rm -fv /var/www/splynx/config/redis.php >>/var/www/splynx/docker/log.log 2>&1
+    dpkg-reconfigure -fnoninteractive splynx >>/var/www/splynx/docker/log.log 2>&1
+}
--- current/start.sh
+++ current/start.sh
@@ -5,11 +5,12 @@ chown mysql:adm /var/log/mysql/error.log >>/var/www/splynx/docker/log.log 2>&1
 chown mysql:adm /var/log/mysql >>/var/www/splynx/docker/log.log 2>&1
 
 #if license is not set - try to set license
-license_is_set=`grep license /var/www/splynx/config/config.php 2>/dev/null | wc -l`
-if [ $license_is_set -eq "0" ]; then
+license_length=`grep license /var/www/splynx/config/config.php | grep -v ^# | tail -n 1 | cut -d= -f2 | tr -d "\n\r\" " | wc -m`
+if ! [ $license_length -eq "32" ]; then
     echo "splynx splynx/license string $1" | debconf-set-selections -v >>/var/www/splynx/docker/log.log 2>&1
-    cp -fv /var/www/splynx/docker/splynx-start.service /etc/systemd/system/multi-user.target.wants/splynx-start.service >>/var/www/splynx/docker/log.log 2>&1
 fi
+
+cp -fv /var/www/splynx/docker/splynx-start.service /etc/systemd/system/multi-user.target.wants/splynx-start.service >>/var/www/splynx/docker/log.log 2>&1
 
 #start system
 exec /lib/systemd/systemd
