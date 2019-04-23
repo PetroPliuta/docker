8377c429a96666957a0d388fb4abc13abc286319
Peter Pliuta <peter@splynx.com.ua>
2019-04-23 12:28:33 +0300

    tests

--- current/tests.sh
+++ current/tests.sh
@@ -2,8 +2,8 @@
 
 splynx_package_apt_status=`apt-cache show splynx | awk '/Status/ {print $NF}'`
 
-if [ "$splynx_package_apt_status" = "installed" ]; then
-    exit 0
-else
-    exit 13
+if [ "$splynx_package_apt_status" != "installed" ]; then
+    exit 12
 fi
+
+#exit 0
