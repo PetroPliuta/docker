92e370cc101fb9f69a721d345b0ac1d42966a4d3
Peter Pliuta <peter@splynx.com.ua>
2020-02-05 18:01:32 +0200

    refactor. simplify

--- 3.0/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp -fv /var/www/splynx/docker/splynx-start.service /etc/systemd/system/multi-user.target.wants/splynx-start.service >>/var/www/splynx/docker/log.log 2>&1
-
-#start system
-exec /lib/systemd/systemd
