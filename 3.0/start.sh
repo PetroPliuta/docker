55e55f7d9f042b23aeebbbea33a9581ff66e2853
Peter Pliuta <peter@splynx.com.ua>
2020-02-05 10:30:52 +0200

    3.0 fixes

--- 3.0/start.sh
+++ 3.0/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #fix mysql log permissions
-chown mysql:adm /var/log/mysql/error.log >>/var/www/splynx/docker/log.log 2>&1
-chown mysql:adm /var/log/mysql >>/var/www/splynx/docker/log.log 2>&1
+chown -v mysql:adm /var/log/mysql/error.log >>/var/www/splynx/docker/log.log 2>&1
+chown -v mysql:adm /var/log/mysql >>/var/www/splynx/docker/log.log 2>&1
 
 #if license is not set - try to set license
 license_length=`grep license /var/www/splynx/config/config.php | grep -v ^# | tail -n 1 | cut -d= -f2 | tr -d "\n\r\" " | wc -m`
